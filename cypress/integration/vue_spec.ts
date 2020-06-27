aa0545379e9ccd3c60d0175e733ec1945fccb1ad
Danilo Velasquez Urrutia <danilo.velasquez@gmail.com>
2020-06-27 16:02:31 +0200

    docs(update-docs): update docs for the other projects (#15)

--- cypress/integration/vue_spec.ts
+++ cypress/integration/vue_spec.ts
@@ -1,10 +1,10 @@
 describe('Vue Component', () => {
   before(() => {
-    cy.visit('/packages/vue-lighthouse-viewer/demo/index.html');
+    cy.visit('/packages/vue-lighthouse-viewer/demo/lh-demo.html');
   });
   it('Check heading categories results', () => {
     cy.get('h1').contains('Vue 2 APP');
-    cy.get('.lh-scores-header > [href="#performance"] > .lh-gauge__percentage').contains(89);
+    cy.get('.lh-scores-header > [href="#performance"] > .lh-gauge__percentage').contains(100);
     cy.get('.lh-scores-header > [href="#accessibility"] > .lh-gauge__percentage').contains(97);
     cy.get('.lh-scores-header > [href="#best-practices"] > .lh-gauge__percentage').contains(100);
     cy.get('.lh-scores-header > [href="#seo"] > .lh-gauge__percentage').contains(98);
@@ -13,12 +13,12 @@ describe('Vue Component', () => {
   it('Check performance metrics', () => {
     cy.get(
       ':nth-child(1) > .lh-category > .lh-category-header > .lh-score__gauge > .lh-gauge__wrapper > .lh-gauge__percentage',
-    ).contains(89);
-    cy.get('#first-contentful-paint > .lh-metric__innerwrap > .lh-metric__value').contains('1.8 s');
-    cy.get('#speed-index > .lh-metric__innerwrap > .lh-metric__value').contains('2.8 s');
-    cy.get('#largest-contentful-paint > .lh-metric__innerwrap > .lh-metric__value').contains('3.5 s');
-    cy.get('#interactive > .lh-metric__innerwrap > .lh-metric__value').contains('3.8 s');
-    cy.get('#total-blocking-time > .lh-metric__innerwrap > .lh-metric__value').contains('130 ms');
+    ).contains(100);
+    cy.get('#first-contentful-paint > .lh-metric__innerwrap > .lh-metric__value').contains('0.9 s');
+    cy.get('#speed-index > .lh-metric__innerwrap > .lh-metric__value').contains('1.8 s');
+    cy.get('#largest-contentful-paint > .lh-metric__innerwrap > .lh-metric__value').contains('0.9 s');
+    cy.get('#interactive > .lh-metric__innerwrap > .lh-metric__value').contains('1.0 s');
+    cy.get('#total-blocking-time > .lh-metric__innerwrap > .lh-metric__value').contains('20 ms');
     cy.get('#cumulative-layout-shift > .lh-metric__innerwrap > .lh-metric__value').contains('0');
   });
   it('Check Accessibility metrics', () => {
@@ -43,12 +43,12 @@ describe('Vue Component', () => {
     cy.get(':nth-child(6) > .lh-env__description').contains('4x slowdown (Simulated)');
     cy.get(':nth-child(7) > .lh-env__description').contains('cli');
     cy.get(':nth-child(8) > .lh-env__description').contains(
-      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4184.0 Safari/537.36',
     );
     cy.get(':nth-child(9) > .lh-env__description').contains(
-      'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3963.0 Mobile Safari/537.36 Chrome-Lighthouse',
+      'Mozilla/5.0 (Linux; Android 7.0; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4143.7 Mobile Safari/537.36 Chrome-Lighthouse',
     );
-    cy.get(':nth-child(10) > .lh-env__description').contains('1366');
-    cy.get('.lh-footer__version').contains('6.0.0');
+    cy.get(':nth-child(10) > .lh-env__description').contains('2018');
+    cy.get('.lh-footer__version').contains('6.1.0');
   });
 });
