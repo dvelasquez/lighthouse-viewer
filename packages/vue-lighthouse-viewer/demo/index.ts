635aafaf8cfbd12809c7ce382cd1e9a5e1e9a3b9
Danilo Velasquez <danilo.velasquez@gmail.com>
2020-06-30 08:10:05 +0200

    fix(vue-component): fixed vue component template
    Since was impossible to make @rollup/plugin-typescript work with rollup-plugin-vue, I took the decision of use a render function and manually add the DOM.

--- packages/vue-lighthouse-viewer/demo/index.ts
+++ packages/vue-lighthouse-viewer/demo/index.ts
@@ -1,24 +1,9 @@
 import Vue from 'vue';
-import VueLighthouseViewer from '..';
-import reportJson from './report.json';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import App from './App.vue';
 
 const vueAppElement = document.getElementById('vue-app');
 if (vueAppElement) {
-  const vueApp = new Vue({
-    el: '#vue-app',
-    name: 'VueApp',
-    template: `
-      <div>
-        <h1>Vue 2 APP</h1>
-        <VueLighthouseViewer v-if="json" :json="json" />
-      </div>`,
-    components: {
-      VueLighthouseViewer,
-    },
-    data: () => {
-      return {
-        json: reportJson,
-      };
-    },
-  });
+  new Vue(App).$mount('#vue-app');
 }
