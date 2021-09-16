7cf2f917ac3d3d1d3a75a0621a848f12344a1db7
Vwings <vwings17@gmail.com>
2021-09-16 16:55:00 +0800

    feat(demo): angular lighthouse viewer demo

--- packages/ngx-lighthouse-viewer/projects/demo/src/app/app.component.ts
+++ packages/ngx-lighthouse-viewer/projects/demo/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import reportJson2 from '../assets/report-v7.5.0.json';
+import reportJson1 from '../assets/report-v8.0.0.json';
+import reportJson3 from '../assets/report.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo';
+  
+  data: any;
+
+  private _reports: any[] = [];
+
+  ngOnInit() {
+    this._reports = [reportJson1, reportJson2, reportJson3];
+    this.data = this._reports[0];
+  }
+
+  renderReport(index: number) {
+    this.data = this._reports[index];
+  }
+  
 }
--- packages/ngx-lighthouse-viewer/projects/demo/src/app/app.module.ts
+++ packages/ngx-lighthouse-viewer/projects/demo/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgxLighthouseViewerModule } from '../../../ngx-lighthouse-viewer/src/lib/ngx-lighthouse-viewer.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,8 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, NgxLighthouseViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
