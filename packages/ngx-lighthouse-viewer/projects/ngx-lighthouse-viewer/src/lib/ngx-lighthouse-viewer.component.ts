b61c53b04473b188dcd366b313ee88f0d8b58501
Vwings <vwings17@gmail.com>
2021-09-16 16:50:02 +0800

    feat(component): angular lighthouse viewer component

--- packages/ngx-lighthouse-viewer/projects/ngx-lighthouse-viewer/src/lib/ngx-lighthouse-viewer.component.ts
+++ packages/ngx-lighthouse-viewer/projects/ngx-lighthouse-viewer/src/lib/ngx-lighthouse-viewer.component.ts
@@ -1,20 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { AfterViewInit, Component, Inject, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { DOM, Logger, ReportRenderer, ReportUIFeatures, template } from 'lighthouse-viewer';
 
 @Component({
-  selector: 'lib-ngx-lighthouse-viewer',
+  selector: 'ngx-lighthouse-viewer',
   template: `
-    <p>
-      ngx-lighthouse-viewer works!
-    </p>
+    <div class="lh-root lh-vars">
+      <div [innerHTML]="template"></div>
+      <main class="ngx-lighthouse-viewer"></main>
+      <div id="lh-log"></div>
+    </div>
   `,
-  styles: [
-  ]
+  styles: [],
 })
-export class NgxLighthouseViewerComponent implements OnInit {
+export class NgxLighthouseViewerComponent implements AfterViewInit, OnChanges {
+  @Input() json: any;
 
-  constructor() { }
+  template;
 
-  ngOnInit(): void {
+  private _viewInit: boolean = false;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    this.template = template;
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    this.generateReport();
+  }
+
+  ngAfterViewInit(): void {
+    this._viewInit = true;
+    this.lhLogEventListener();
+    this.generateReport();
+  }
+
+  lhLogEventListener = () => {
+    this.document.addEventListener('lh-log', (e: CustomEventInit) => {
+      const logger = new Logger(this.document.querySelector('#lh-log'));
+      switch (e.detail.cmd) {
+        case 'log':
+          logger.log(e.detail.msg);
+          break;
+        case 'warn':
+          logger.warn(e.detail.msg);
+          break;
+        case 'error':
+          logger.error(e.detail.msg);
+          break;
+        case 'hide':
+          logger.hide();
+          break;
+        default:
+      }
+    });
+  };
+
+  generateReport() {
+    if (!this._viewInit || !this.json) {
+      return;
+    }
+
+    const dom = new DOM(this.document);
+    const renderer = new ReportRenderer(dom);
+
+    const container = this.document.querySelector('main.ngx-lighthouse-viewer');
+
+    renderer.renderReport(this.json, container);
+
+    const features = new ReportUIFeatures(dom);
+    features.initFeatures(this.json);
+  }
 }
