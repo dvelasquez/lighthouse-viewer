71e21808112ca03ec05ae7a20258448bb4e59e07
Vwings <vwings17@gmail.com>
2021-09-22 19:22:08 +0800

    feat(angular build): remove lighthouse viewer service, add dependency.
    add lighhthouse-viewer in workspace

--- packages/ngx-lighthouse-viewer/projects/ngx-lighthouse-viewer/src/public-api.ts
+++ packages/ngx-lighthouse-viewer/projects/ngx-lighthouse-viewer/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of ngx-lighthouse-viewer
  */
 
-export * from './lib/ngx-lighthouse-viewer.service';
 export * from './lib/ngx-lighthouse-viewer.component';
 export * from './lib/ngx-lighthouse-viewer.module';
+
