1c8b884452c37e554fd184a550d897c568d5d18b
Danilo Velasquez Urrutia <danilo.velasquez@gmail.com>
2024-12-22 14:50:50 +0100

    Update nextjs to v14 (#1597)
    * feat: upgraded nextjs to v14
    
    * chore: updated nextjs version
    
    * chore: update package.json

--- examples/nextjs-ts/pages/index.tsx
+++ examples/nextjs-ts/pages/index.tsx
@@ -22,9 +22,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.center}>
           <Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
-          <div className={styles.thirteen}>
-            <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-          </div>
+          
         </div>
 
         <div className={styles.grid}></div>
